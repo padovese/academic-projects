174ab250d50cb06debf0bbba39cdb10dcb1373d4
padovese <bhspadoveze@gmail.com>
2018-04-18 22:38:42 -0300

    evolving

--- R Course/modulo 2/modulo2.R
+++ R Course/modulo 2/modulo2.R
@@ -1,8 +1,14 @@
 rm(list=ls()) ##Remove tudo feito anteriormente
 
+install.packages("plyr") ##instala
+library(plyr) ##import
+
 duracao <- rename(duracao, replace = c("user_id"="aluno", "course_id"="curso", "timeToFinish"="dias")) ##renomeia
 
 plot(duracao$dias) ##Cria grafico, porem R escolhe o tipo do grafico
 
-hist(duracao$dias) ##Cria grafico especifico do tipo tabela coluna
+jpeg("histograma.jpg")
 
+hist(duracao$dias, breaks = 20, main="Histograma do Tempo", ylab="Quantidade", xlab="Dias", ylim=c(0, 2000), col="blue") ##Cria grafico especifico do tipo tabela coluna, customizando-o
+
+dev.off()
