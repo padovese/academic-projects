fc258f2398233e6ba0c444ce831b3d26557effd8
padovese <bhspadoveze@gmail.com>
2018-04-23 00:18:12 -0300

    adding maps

--- ny-crimes-prediction/full.R
+++ ny-crimes-prediction/full.R
@@ -19,3 +19,41 @@ plot(crime$hora, ylim=c(0, 85000))
 barplot(sort(table(crime$descricao), decreasing=T), ylim=c(0, 2000))
 barplot(sort(table(crime$bairro), decreasing=T), ylim=c(0, 3000),  col=c("yellow", "green", "red", "orange", "purple"))
 barplot(sort(table(crime$status), decreasing=T), ylim=c(0, 10000))
+
+# mapa de NYC
+install.packages("ggmap")
+library(ggmap)
+mymap <- get_map(location = "New York", maptype = "roadmap")
+ggmap(mymap)
+
+# consultas em SQL
+install.packages("sqldf")
+library(sqldf)
+
+furto <- sqldf('select CMPLNT_NUM, latitude, longitude, lat_lon from crime where descricao = upper("petit larceny")')
+assalto <- sqldf('select CMPLNT_NUM, latitude, longitude, lat_lon from crime where descricao like upper("%assault%")')
+fraud <- sqldf('select CMPLNT_NUM, latitude, longitude, lat_lon from crime where descricao like upper("%fraud%")')
+qtde <- sqldf('select descricao, count(*) qtde from crime group by descricao order by 2 desc')
+
+# crimes mapeados
+ggmap(mymap)+
+  geom_point(aes(x = Longitude, y = Latitude), data = crime, 
+             alpha = .5, color="darkred", size = 1)
+
+# furtos mapeados
+ggmap(mymap)+
+  geom_point(aes(x = Longitude, y = Latitude), data = furto, 
+             alpha = .5, color="darkred", size = 1)
+
+# assalto mapeados
+ggmap(mymap)+
+  geom_point(aes(x = Longitude, y = Latitude), data = assalto, 
+             alpha = .5, color="darkred", size = 1)
+
+# fraldes mapeados
+ggmap(mymap)+
+  geom_point(aes(x = Longitude, y = Latitude), data = fraud, 
+             alpha = .5, color="darkred", size = 1)
+
+
+
